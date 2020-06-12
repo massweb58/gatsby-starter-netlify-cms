97e9f93a533813fc05ed82997bfe78fdcd91e40a
massweb <massweb@outlook.fr>
2020-06-12 11:06:38 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="fsdfsdfsdfsd">
+            <Link to="/" className="navbar-item" title="logo MassWeb">
               <img src={logo} alt="MassDev" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
