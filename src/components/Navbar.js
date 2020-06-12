4bd7c89aa7966c06b410b4a7b07c53d3cc59a46d
massweb <massweb@outlook.fr>
2020-06-12 17:48:19 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,20 +61,20 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                A Propos
               </Link>
-              <Link className="navbar-item" to="/products">
+              {/*<Link className="navbar-item" to="/products">
                 Products
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
+              </Link>*/}
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contactez-nous
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              {/*<Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+            </Link>*/}
             </div>
             <div className="navbar-end has-text-centered">
               <a
