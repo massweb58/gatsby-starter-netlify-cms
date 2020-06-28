909836ac7051d67fa2b0a55da57c7c9c0e749fe8
massweb <massweb@outlook.fr>
2020-06-28 17:53:39 +0200

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,18 +63,18 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 A Propos
               </Link>
-              {/*<Link className="navbar-item" to="/products">
+              <Link className="navbar-item" to="/products">
                 Products
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>*/}
+              </Link>
               <Link className="navbar-item" to="/contact">
                 Contactez-nous hdhsbdhsg
               </Link>
-              {/*<Link className="navbar-item" to="/contact/examples">
+              <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-            </Link>*/}
+            </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
